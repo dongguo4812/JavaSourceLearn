02692d0aadd06f857892462dfed8186066bc5daa
dongguo4812 <291320608@qq.com>
2021-08-19 22:52:58 +0800

    JavaSourceLearn初始提交

---
+++ src/main/java/test/Test.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Dongguo
+ * @date 2021/8/19 0019-19:05
+ * @description:
+ */
+public class Test {
+    public static void main(String[] args) {
+        Map<Integer,String> hashMap = new HashMap<>();
+        hashMap.put(1,"张三");
+        hashMap.put(2,"李四");
+        hashMap.put(3,"王五");
+    }
+}
