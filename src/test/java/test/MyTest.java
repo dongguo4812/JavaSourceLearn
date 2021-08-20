cf40ff4fe729e99a7631f4e1b6b4c596933dc540
dongguo4812 <291320608@qq.com>
2021-08-20 20:46:48 +0800

    无

--- src/test/java/test/MyTest.java
+++ src/test/java/test/MyTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 
@@ -12,11 +13,11 @@ import java.util.List;
  * @date 2021/8/20 0020-9:47
  * @description:
  */
-class TestTest {
+class MyTest {
     @Test
     public void test1() {
 
-        String array[]= {"hello","world","java"};
+        String array[] = {"hello", "world", "java"};
 //        List<String> list1 = Arrays.asList(array);
 //        list1.add("zhangsan");
 //        System.out.println(list1);
@@ -27,5 +28,14 @@ class TestTest {
         System.out.println(list);
         list.remove("zhangsan");
         System.out.println(list);
+    }
+
+    @Test
+    public void test2() {
+        HashMap hashMap = new HashMap();
+        hashMap.put(1, "1");
+        hashMap.put(1, "1");
+        hashMap.put(11, "11");
+        System.out.println(hashMap);
     }
 }
