6a3124be545c6f7ce4aa2037a315efa6f0e99e64
dongguo <291320608@qq.com>
2023-05-21 07:24:07 +0800

    Object源码.md

--- src/test/java/test/java/lang/ObjectTest.java
+++ src/test/java/test/java/lang/ObjectTest.java
@@ -1,12 +1,15 @@
 package test.java.lang;
 
+import org.junit.jupiter.api.Test;
+
 /**
  * @author dongguo
  * @date 2023/4/17
  * @description:
  */
 public class ObjectTest {
+    @Test
     public void test(){
-     new Object();
+        Object o = new Object();
     }
 }
